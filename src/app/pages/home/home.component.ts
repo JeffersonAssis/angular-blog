c215d321319d7766ebe4172ddaaca5d9926f4671
Jefferson Assis <jbrunoassis@gmail.com>
2024-01-24 01:04:34 -0300

    Criado os components

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { SmallCardComponent } from '../../components/small-card/small-card.component';
+import { TituloComponent } from '../../components/titulo/titulo.component';
+import { MenuComponent } from '../../components/menu/menu.component';
+import { BigCardComponent } from '../../components/big-card/big-card.component';
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [BigCardComponent, TituloComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
